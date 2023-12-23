f32c5a235521efad14909340c97efb835a7407c3
Ian Covaliuc <ian.covaliuc@gmail.com>
2023-12-23 16:54:21 +0200

    Get rid of React.memo

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -1,22 +1,24 @@
 import './ChatWindow.css';
 
 import PropTypes from "prop-types";
-import React from "react";
 
-const ChatWindow = React.memo(({messages}) => {
-    const messageHistory = messages.map((message, index) => (
-        <p key={index} className={'message'}>{message}</p>
-    ));
+function ChatWindow({messages}) {
+
+    const messageHistory = messages.map((message, index) => {
+        return (
+            <p key={index} className={'message'}>{message}</p>
+        )
+    });
 
     return (
         <div className={'chat-window'}>
             {messageHistory}
         </div>
-    );
-});
+    )
+}
 
 ChatWindow.propTypes = {
     messages: PropTypes.arrayOf(PropTypes.string)
-};
+}
 
 export default ChatWindow;
